ad0c4d629f1eeb061ebe65ad8fe72617b4e7ccc2
ManaswibRane <manaswirane1947@gmail.com>
2025-02-19 12:02:46 +0530

    x

--- JS/part 7/app.js
+++ JS/part 7/app.js
@@ -7,7 +7,36 @@
 //         let avg=(this.maths+this.phy+this.sci)/3;
 //         console.log(`${this.name} average marks is ${avg}`);
 //     },
-    
+
 // }
 // student.getAvg();
 //This object executing current piece of code
+//ERROR HANDLING
+// console.log("Hello");
+// try{
+//     console.log(a);
+// }
+// catch(e){
+//     console.log("Error detected");
+//     console.log(e);
+// }
+// console.log("Hello");
+// console.log("Hello");
+const sum = (a, b) => {
+    console.log(a + b);
+}
+const square = x => {
+    console.log(x ** 2);
+}
+const run=n=>n**3;
+const cube = (n) => (n * n * n);
+setTimeout(()=>{
+    console.log("Hello World")
+},4000);
+let id=setInterval(()=>{ //executes after every 4 seconds
+    console.log("Hello World",id)
+},4000)   // clearInterva(id)
+clearInterval(2)
+//THIS KEYWORD
+// arrow function-> lexographical Scope parent ko kisne call kiya hai
+// function-> calling object
